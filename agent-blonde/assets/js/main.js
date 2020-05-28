dbb51477f37c2613f6d81ab0862613f6b9ad4a8b
valentinzm <valentine16px@gmail.com>
2020-05-28 22:25:14 +0300

    agent blode 3

--- agent-blonde/assets/js/main.js
+++ agent-blonde/assets/js/main.js
@@ -172,3 +172,24 @@ function readMore() {
         e.classList.remove('read-more__text');
     })
 }
+var countDownDate = new Date("Sep 1, 2020 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = '<p>' + days + "<span>дней</span></p><p>" + hours + "<span>часов<span>дней</span></p><p> " +
+        minutes + "<span>минут<span>дней</span></p><p> " + seconds + "<span>секунд<span>дней</span></p><p> ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
