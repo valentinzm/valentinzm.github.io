97fb106c9338564514b6df2a25ef7feda00585ed
valentinzm <valentine16px@gmail.com>
2020-08-05 06:35:58 +0300

    snedok

---
+++ snedok/scripts/main.js
@@ -0,0 +1,77 @@
+var swiper = new Swiper('.front-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+let swiperOffers = new Swiper('.offers__slider', {
+    slidesPerView: 4,
+    loop: true,
+    spaceBetween: 25,
+})
+document.querySelector('.catalog__button').addEventListener('click', function(e) {
+    document.querySelector('.catalog__content').classList.toggle('catalog__show');
+});
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    document.querySelectorAll('.products__item').forEach(function(e, idx) {
+        e.id = 'product' + idx;
+    })
+    document.querySelectorAll('.products__add').forEach(function(btn, i) {
+        btn.dataset.target = '#product' + i;
+    });
+});
+
+
+document.querySelectorAll('.products-minus').forEach(function(e) {
+    e.addEventListener('click', function() {
+        let cardQty = this.nextElementSibling.value;
+        if (cardQty != 1) {
+            cardQty--;
+        }
+    });
+});
+
+
+// document.querySelector('.js-plus').addEventListener('click', function() {
+//     qty.value++;
+// });
+
+document.querySelectorAll('.products__add').forEach(function(e) {
+    e.addEventListener('click', addProduct);
+});
+
+function addProduct(event) {
+    event.preventDefault();
+    let productId = this.dataset.target;
+    let product = document.querySelector(productId);
+    this.previousElementSibling.remove();
+    this.remove();
+
+
+    product.classList.add('products__added');
+}
+
+//modals
+document.querySelectorAll('.js-modal').forEach((e) => e.addEventListener('click', showModal));
+
+function showModal(event) {
+    event.preventDefault();
+    let modal = this.dataset.modal;
+    document.querySelector(modal).classList.add('modal__show');
+    document.onkeydown = function(e) {
+        if (e.keyCode == 27) {
+            document.querySelector(modal).classList.remove('modal__show');
+        }
+    }
+}
+
+document.querySelectorAll('.modal__close').forEach((e) => e.addEventListener('click', closeModals));
+document.querySelectorAll('.modal__overlay').forEach((e) => e.addEventListener('click', closeModals));
+
+function closeModals(evt) {
+    evt.preventDefault();
+    document.querySelectorAll('.modal').forEach(function(e) {
+        e.classList.remove('modal__show');
+    })
+}
---
+++ snedok/scripts/single-product.js
@@ -0,0 +1,47 @@
+const qty = document.querySelector('.single-product__input');
+document.querySelector('.js-minus').addEventListener('click', function() {
+    if (qty.value != 1) {
+        qty.value--;
+    }
+});
+document.querySelector('.js-plus').addEventListener('click', function() {
+    qty.value++;
+});
+
+
+
+var galleryThumbs = new Swiper('.single-product__thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.single-product__top', {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    }
+});
+
+
+document.querySelectorAll('.tabs__item').forEach(function(e) {
+    e.addEventListener('click', productTabs);
+});
+
+function productTabs(event) {
+    event.preventDefault();
+    document.querySelectorAll('.tabs__item').forEach(function(e) {
+        e.classList.remove('tabs__item_active');
+    });
+    this.classList.add('tabs__item_active');
+    let tab = this.dataset.tab;
+    // document.querySelectorAll('tabs__content').forEach(function(e) {
+    //     e.classList.remove('tabs__content_active');
+    // });
+    document.querySelector(tab).classList.add('tabs__content_active');
+}
