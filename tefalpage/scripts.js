ed01fa66f1e90b494cebf3a72ade6a219ceeecef
valentinzm <valentine16px@gmail.com>
2020-08-10 09:56:32 +0300

    tefal 1

--- tefalpage/scripts.js
+++ tefalpage/scripts.js
@@ -48,3 +48,63 @@ document.addEventListener('keydown', function(event) {
         });
     }
 });
+
+document.querySelector('.smart__more').addEventListener('click', showSmartSlide);
+
+function showSmartSlide(evt) {
+    evt.preventDefault();
+    const slide = document.querySelector('.smart__slide');
+    if (slide.classList.contains('smart__slide--on')) {
+        slide.classList.add('smart__slide--off');
+        slide.classList.remove('smart__slide--on');
+    } else {
+        slide.classList.add('smart__slide--on');
+        slide.classList.remove('smart__slide--off');
+    }
+    // slide.classList.add('smart__slide--on');
+    // this.textContent = 'Скрыть';
+    // if (slide.classList.contains('smart__slide--on')) {
+    //     slide.classList.add('smart__slide--off');
+
+    // } else {
+    //     slide.classList.add('smart__slide--on');
+
+
+    // }
+
+}
+
+
+document.querySelectorAll('.pick__button').forEach(function(e) {
+    e.addEventListener('click', filter);
+});
+
+function filter(event) {
+    event.preventDefault();
+    this.classList.add('pick__button--active');
+}
+document.querySelector('.pick__reset').addEventListener('click', resetFiler);
+
+function resetFiler(event) {
+    event.preventDefault();
+    document.querySelectorAll('.pick__button').forEach(function(e) {
+        e.classList.remove('pick__button--active');
+    });
+}
+
+
+
+jQuery('.pick__show-btn--open').on("click", function(event) {
+
+    event.preventDefault();
+    $(".details").slideDown();
+    $(this).slideDown().hide();
+
+});
+jQuery('.pick__show-btn--close').on("click", function(event) {
+
+    event.preventDefault();
+    $(".details").slideUp();
+    $('.pick__show-btn--open').show();
+
+});
