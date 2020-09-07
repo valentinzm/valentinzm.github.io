4e6aec782425e4aa8a7e71f9ae5c7debd9f3f2a4
valentinzm <valentine16px@gmail.com>
2020-09-07 21:22:41 +0300

    icons02

--- russian-icons/scripts/app.js
+++ russian-icons/scripts/app.js
@@ -2,13 +2,31 @@ const zenSlider = new Swiper('.zen__slider', {
     // Optional parameters
 
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 28,
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 0,
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+        768: {
+            centeredSlides: false,
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        992: {
+            centeredSlides: true,
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        1240: {
+            centeredSlides: true,
+            slidesPerView: 3,
+            spaceBetween: 28,
+        },
+    }
 
 });
 const rewiewsSlider = new Swiper('.rewiews__slider', {
@@ -37,4 +55,19 @@ function faqList(evt) {
     parent.classList.toggle('faq__item--open');
 
     //content.style.display = 'block';
+}
+// window.addEventListener('scroll', function() {
+//     let scrollValue = window.scrollY;
+//     if (scrollValue >= 110) {
+//         document.querySelector('.navigation').classList.add('navigation__sticky');
+//     } else {
+//         document.querySelector('.navigation').classList.remove('navigation__sticky');
+//     }
+// })
+document.querySelector('.burger').addEventListener('click', mobileNavigation);
+
+function mobileNavigation(evt) {
+    evt.preventDefault();
+    this.classList.toggle('burger__open');
+    document.querySelector('.navigation').classList.toggle('navigation__open');
 }
